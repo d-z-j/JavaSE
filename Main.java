06ca4077c689a882e7ad145d438b4dbf76d9ef70
记录学习生活sisyphe <101461459+d-z-j@users.noreply.github.com>
2022-06-07 22:14:17 +0800

    2022.6.7

--- Main.java
+++ Main.java
@@ -1,12 +1,34 @@
+package P58;
+
 public class Main {
     public static void main(String[] args) {
-
+        TreeNode<String> root = new TreeNode<>("A");
+        root.left = new TreeNode<>("B");
+        root.right = new TreeNode<>("E");
+        root.left.left = new TreeNode<>("C");
+        root.left.right = new TreeNode<>("D");
+        root.right.right = new TreeNode<>("F");
+        test(root);
     }
-
-    public static void test(Person person){
-        if (person instanceof Exam){
-            Exam e = (Exam) person;
-            e.exam();
-        }
+    //后序遍历
+    private static void test(TreeNode<String> root) {
+        if (root == null) return;;
+        test(root.left);
+        test(root.right);
+        System.out.print(root.e + " ");
     }
+    //中序遍历
+    /**private static void test(TreeNode<String> root) {
+        if (root == null) return;;
+        test(root.left);
+        System.out.print(root.e + " ");
+        test(root.right);
+    }**/
+    //先序遍历
+    /**private static void test(TreeNode<String> root){
+        if (root == null) return;;
+        System.out.print(root.e + " ");
+        test(root.left);
+        test(root.right);
+    }**/
 }
---
+++ TreeNode.java
@@ -0,0 +1,11 @@
+package P58;
+
+public class TreeNode<E> {
+    public E e;
+    public TreeNode<E> left;
+    public TreeNode<E> right;
+
+    public TreeNode(E e){
+        this.e = e;
+    }
+}
